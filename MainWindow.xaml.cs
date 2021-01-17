223cf9143c7a24fee2bb555e98316ea70783e5fb
Mark Bolotskih <markbolotskih@ya.ru>
2021-01-17 22:32:43 +0300

    Добавил функцию создания вкладок

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TimerWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AddNewTab();
+        }
+
+        private void AddTab_Click(object sender, MouseButtonEventArgs e)
+        {
+            AddNewTab();
+        }
+
+        private void AddNewTab()
+        {
+            var currentTime = DateTime.Now.ToString("HH:mm");
+            int index = TimerTabControl.Items.Count;
+            TimerTabControl.Items.Insert(index - 1, new TabItem
+            {
+                Header = new TextBlock { Text = $"{index} {currentTime}" }, // установка заголовка вкладки
+            });
+
+            if (index == 10)
+            {
+                AddTab.Visibility = Visibility.Hidden;               
+            }
+        }
+    }
+}
+
