23f16173fefe00803ab955514cbffb0f766d363c
Mark Bolotskih <markbolotskih@ya.ru>
2021-02-01 17:56:05 +0300

    Переименовал поля

--- Models/TimerTabModel.cs
+++ Models/TimerTabModel.cs
@@ -5,11 +5,11 @@ namespace TimerWpfApp.Models
 {
     public class TimerTabModel : Tab
     {
-        public TabItemViewModel DateTabViewModel { get; }
+        public TabItemViewModel TabItemViewModel { get; }
         public TimerTabModel()
         {
             Name = DateTime.Now.ToString("HH:mm:ss");
-            DateTabViewModel = new TabItemViewModel();
+            TabItemViewModel = new TabItemViewModel();
         }
     }
 }
